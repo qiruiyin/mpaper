aaf4d4d21522e4bddac4f37624c93420303a9356
qiruiyin <qiruiyin@126.com>
2016-11-09 13:28:14 +0800

    一期微调

--- app/js/app.ts
+++ app/js/app.ts
@@ -1,24 +1,23 @@
 (function(){
 	var catalog, // 栏目信息
 		catalog_detail = [],
-		banner_info; // 栏目信息列表
-	$.ajax({
-		type: "get",
-		url: "http://api.ch-jht.com/Carousel",
-		dataType : "jsonp",
-		jsonp: "callback",
-		success: function(data){
-			banner_info = data.Data[0];
-			getCatalog();
-		}
-	});
+		dth = 0; // 栏目信息列表
+	var weekday = ['日', '一', '二', '三', '四', '五', '六'],
+		date = new Date(),
+		year = date.getFullYear(),
+		month = date.getMonth() + 1,
+		day = date.getDate(),
+		week = date.getDay();
+	$("title").html("海棠早班车[" + year + "." + month + "." + day + " 星期" + weekday[week] + "]");
+
+	getCatalog();
 	var tabs = function(data_title, data_content){
 		new Vue({
 			el: '#mpaper',
 			data: {
 				iscur: 0,
 				isShow: -1,
-				banner_info: banner_info,
+				dth: dth,
 				titles: data_title,
 				contents: data_content
 			},
@@ -30,15 +29,16 @@
 				    _this.isShow = -1;
 
 				    if (catalog_detail[id] != undefined) {
+						_this.dth = id == 43 ? 1 : 0;
 						_this.contents = catalog_detail[id];
 				    } else {
 				    	$.ajax({
 							type: "get",
-							url: "http://api.ch-jht.com/Detail/" + catalog.Data[i].id,
-							dataType : "jsonp",
-							jsonp: "callback",
+							url: "http://api.ch-jht.com/api/Detail/read/id/" + catalog.Data[i].id,
+							dataType : "json",
 							success: function(data){
 								var id = catalog.Data[i].id;
+								_this.dth = id == 43 ? 1 : 0;
 								catalog_detail[id] = data.Data;
 					    		_this.contents = data.Data;
 							}
@@ -54,18 +54,14 @@
 	function getCatalog(){
 		$.ajax({
 			type: "get",
-			url: "http://api.ch-jht.com/Menu",
-			dataType : "jsonp",
-			jsonp: "callback",
-			async: false,
+			url: "http://api.ch-jht.com/api/Menu",
+			dataType : "json",
 			success: function(result){
 				catalog = result;
 				$.ajax({
 					type: "get",
-					url: "http://api.ch-jht.com/Detail/" + catalog.Data[0].id,
-					dataType : "jsonp",
-					jsonp: "callback",
-					async: false,
+					url: "http://api.ch-jht.com/api/Detail/read/id/" + catalog.Data[0].id,
+					dataType : "json",
 					success: function(data){
 						var id = catalog.Data[0].id;
 						catalog_detail[id] = data.Data;
@@ -78,6 +74,4 @@
 			}
 		});
 	}
-	
 })();
-
--- gulpfile.js
+++ gulpfile.js
@@ -71,7 +71,7 @@
 		        outputStyle: 'expanded',
 		        includePaths: [ './bower_components/' ]
 		    }))
-	      	.pipe($.cssSpriter())
+	      	// .pipe($.cssSpriter())
 			.pipe($.autoprefixer({
 				browers: ['last 2 versions','safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']
 			}))
