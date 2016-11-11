b719a592ef1e48e1392e3859b85ba32db5ead496
qiruiyin <qiruiyin@126.com>
2016-11-11 09:36:30 +0800

    早班车二期

---
+++ mpaper/src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+// import App from './App.vue'
+import Mpaper from './Mpaper.vue'
+
+// console.log(Vue);
+
+new Vue({
+  el: '#app',
+  render: h => h(Mpaper)
+})
---
+++ mpaper/webpack.config.js
@@ -0,0 +1,81 @@
+var path = require('path')
+var webpack = require('webpack')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue',
+        options: {
+          // vue-loader options go here
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract("style", 'css')
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract("style", 'css!sass')
+      }
+    ]
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      vue: {
+        
+        postcss: [require('autoprefixer')()]
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue'
+    }
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
